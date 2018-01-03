ada937a25c7d42fc2dadaf9118c6f36224ef28d7
Bertolin <bertolb@8c8590019708.ant.amazon.com>
2018-01-03 12:28:01 +0000

    Moved bash scripts to scripts folder

--- updatePlex.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-Â# Updates the plexmediaserver service with thegiven .rpm package
-
-sudo service plexmediaserver stop
-sudo yum localupdate -y $1
-sudo service plexmediaserver start
-
-echo "Installation complete"
