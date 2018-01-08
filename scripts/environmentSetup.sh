3b33a832d852bcb0c6b057b5100ca8a85a223494
Albert Quiroga <pyro_quiro@hotmail.com>
2018-01-08 12:51:23 +0000

    Added better explanation in environmentSetup

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -39,17 +39,17 @@ echo 'Installing zsh-output-highlighting plugin...'
 git clone https://github.com/l4u/zsh-output-highlighting.git $PLUGINSPATH/zsh-output-highlighting
 
 # .zshrc file modifications
-sed -i 's/robbyrussell/agnoster/g' .zshrc                                           # Change theme to agnoster
-echo '# ZSH syntax highlighting (must be at the end of the file)' >> .zshrc         # Add syntax hightlighting plugin
+sed -i 's/robbyrussell/agnoster/g' .zshrc                                                 # Change theme to agnoster
+echo '# ZSH syntax highlighting (must be at the end of the file)' >> .zshrc               # Add syntax hightlighting plugin
 echo "source $PLUGINSPATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc  # Add syntax hightlighting plugin
-sed -i "/plugins=($/a \ \ zsh-autosuggestions" .zshrc                               # Add zsh-autosuggestions plugin
-sed -i "/plugins=($/a \ \ zsh-output-highlighting" .zshrc                           # Add zsh-output-hightlighting plugin
+sed -i "/plugins=($/a \ \ zsh-autosuggestions" .zshrc                                     # Add zsh-autosuggestions plugin
+sed -i "/plugins=($/a \ \ zsh-output-highlighting" .zshrc                                 # Add zsh-output-hightlighting plugin
 
 
 # Check the username. If it is ec2-user this is probably an AWS machine and needs creating a password 
 # for the user to change shell into ohmyzsh
 if [[ $USERNAME == 'ec2-user' ]]; then
-  echo 'Your username is ec2-user so this is probably an AWS machine. You will need to create a password for the user'
+  echo 'Your username is ec2-user so this is probably an AWS machine. You will need to create a password for the user. This script will automatically delete back the password at the end.'
   sudo passwd ec2-user
 fi
 
