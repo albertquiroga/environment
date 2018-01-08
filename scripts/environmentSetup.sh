526f43dc1c23de05e560f4c01908d547b9bd622d
Albert Quiroga <albertquirogabertolin@gmail.com>
2018-01-08 13:02:09 +0000

    Fixed color formatting

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -10,42 +10,42 @@ NC='\033[0m'        # No Color
 
 # Check if git is installed
 if ! [ -x "$(command -v git)" ]; then
-  echo "${RED}Error: git is not installed.${NC}" >&2
+  printf "${RED}Error: git is not installed.${NC}\n" >&2
   exit 1
 fi
 
 # Check if ZSH is installed
 if ! [ -x "$(command -v zsh)" ]; then
-  echo "${RED}Error: zsh is not installed." >&2
+  printf "${RED}Error: zsh is not installed." >&2
   if [[ $UNAMESTR == 'Linux' ]]; then
-     echo "Can be installed with package manager (yum install zsh)${NC}" >&2
+     printf "Can be installed with package manager (yum install zsh)${NC}\n" >&2
   elif [[ $UNAMESTR == 'Darwin' ]]; then
-     echo "Can be installed with: brew install zsh zsh-completion${NC}" >&2
+     printf "Can be installed with: brew install zsh zsh-completion${NC}\n" >&2
   fi
   exit 1
 fi
 
 # Install Oh My ZSH
-echo "${GREEN}Installing oh-my-zsh...${NC}"
+printf "${GREEN}Installing oh-my-zsh...${NC}\n"
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # Install zsh-autosuggestions plugin
-echo "${GREEN}Installing zsh-autosuggestions plugin...${NC}"
+printf "${GREEN}Installing zsh-autosuggestions plugin...${NC}\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGINSPATH/zsh-autosuggestions
 
 # Install zsh-syntax-highlighting
-echo "${GREEN}Installing zsh-syntax-highlighting plugin...${NC}"
+printf "${GREEN}Installing zsh-syntax-highlighting plugin...${NC}\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGINSPATH/zsh-syntax-highlighting
 
 # Install zsh-output-highlighting
-echo "${GREEN}Installing zsh-output-highlighting plugin...${NC}"
+printf "${GREEN}Installing zsh-output-highlighting plugin...${NC}\n"
 git clone https://github.com/l4u/zsh-output-highlighting.git $PLUGINSPATH/zsh-output-highlighting
 
 # .zshrc file modifications
 sed -i 's/robbyrussell/agnoster/g' .zshrc                                                 # Change theme to agnoster
-echo '# ZSH syntax highlighting (must be at the end of the file)' >> .zshrc               # Add syntax hightlighting plugin
-echo "source $PLUGINSPATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc  # Add syntax hightlighting plugin
+printf '# ZSH syntax highlighting (must be at the end of the file)' >> .zshrc               # Add syntax hightlighting plugin
+printf "source $PLUGINSPATH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> .zshrc  # Add syntax hightlighting plugin
 sed -i "/plugins=($/a \ \ zsh-autosuggestions" .zshrc                                     # Add zsh-autosuggestions plugin
 sed -i "/plugins=($/a \ \ zsh-output-highlighting" .zshrc                                 # Add zsh-output-hightlighting plugin
 
@@ -53,7 +53,7 @@ sed -i "/plugins=($/a \ \ zsh-output-highlighting" .zshrc                       
 # Check the username. If it is ec2-user this is probably an AWS machine and needs creating a password
 # for the user to change shell into ohmyzsh
 if [[ $USERNAME == 'ec2-user' ]]; then
-  echo "${GREEN}Your username is ec2-user so this is probably an AWS machine. You will need to create a password for the user. This script will automatically delete back the password at the end.${NC}"
+  printf "${GREEN}Your username is ec2-user so this is probably an AWS machine. You will need to create a password for the user. This script will automatically delete back the password at the end.${NC}\n"
   sudo passwd ec2-user
 fi
 
@@ -62,9 +62,9 @@ chsh -s /bin/zsh
 
 # Delete user password if this is AWS
 if [[ $USERNAME == 'ec2-user' ]]; then
-  echo "${GREEN}Deleting previously created user password for security reasons...${NC}"
+  printf "${GREEN}Deleting previously created user password for security reasons...${NC}\n"
   sudo passwd --delete ec2-user
 fi
 
 # Final message
-echo "${GREEN}Installation was successful. Relog into the console to enter oh-my-zsh. Keep in mind you have to install highlighting (OSX> brew install highlight Linux> install liblua https://rpmfind.net/linux/rpm2html/search.php?query=liblua-5.3.so()(64bit) , then install highlight http://www.andre-simon.de/zip/download.php.${NC}"
+printf "${GREEN}Installation was successful. Relog into the console to enter oh-my-zsh. Keep in mind you have to install highlighting (OSX> brew install highlight Linux> install liblua https://rpmfind.net/linux/rpm2html/search.php?query=liblua-5.3.so()(64bit) , then install highlight http://www.andre-simon.de/zip/download.php.${NC}\n"
